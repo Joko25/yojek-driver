2d0cb73d6e54305ec9c905f5896d519b75443243
Joko Mulyono <jokomulyono696@gmail.com>
2018-03-26 18:32:56 +0700

    26

---
+++ www/js/route.js
@@ -0,0 +1,61 @@
+app.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+    .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  })
+    .state('login', {
+    url: '/login',
+    // abstract: true,
+    templateUrl: 'templates/login.html',
+    controller: 'loginCtrl'
+  })
+    .state('home', {
+    url: '/home',
+    // abstract: true,
+    templateUrl: 'templates/home.html',
+    controller: 'AppCtrl'
+  })
+
+  .state('app.search', {
+    url: '/search',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/search.html'
+      }
+    }
+  })
+
+  .state('app.browse', {
+      url: '/browse',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/browse.html'
+        }
+      }
+    })
+    .state('app.playlists', {
+      url: '/playlists',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/playlists.html',
+          controller: 'PlaylistsCtrl'
+        }
+      }
+    })
+
+  .state('app.single', {
+    url: '/playlists/:playlistId',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/playlist.html',
+        controller: 'PlaylistCtrl'
+      }
+    }
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/home');
+});
